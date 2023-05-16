4a91a81991727c6e077796ebaca5021a75654b1c
shallowman <feron.wu@qq.com>
2023-05-16 15:19:04 +0800

    chore(.php-cs-fixer.php): remove un-support cs-fixer rule for cs-fixer specified version

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -7,7 +7,6 @@ $finder = PhpCsFixer\Finder::create()
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
-        'single_line_empty_body' => true,
         'modernize_types_casting' => true,
         'no_useless_return' => true,
         'return_assignment' => true,
