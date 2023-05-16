122ac8174cb6f8e935dc16ea3521a9f915724c35
shallowman <feron.wu@qq.com>
2023-05-16 15:13:18 +0800

    chore(.php-cs-fixer.php): more risky cs rule list

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -7,4 +7,12 @@ $finder = PhpCsFixer\Finder::create()
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
+        'single_line_empty_body' => true,
+        'modernize_types_casting' => true,
+        'no_useless_return' => true,
+        'return_assignment' => true,
+        'simplified_null_return' => true,
+        'combine_consecutive_issets' => true,
+        'combine_consecutive_unsets' => true,
+        'logical_operators' => true,
     ])->setFinder($finder);
